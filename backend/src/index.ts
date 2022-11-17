ca9d6d0197b51189f386ca5e119a2a749bd879a6
Håkon <haakon.telje@gmail.com>
2022-11-17 13:04:40 +0100

    endpoint for upload successful

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { initializeApp } from "firebase/app";
+import { readFileSync } from "fs";
 
+/**
+ * Config fil for å uploade til mitt firebase proskjekt sin backend.
+ */
 const firebaseConfig = {
   storageBucket: "gs://beuka-backend.appspot.com",
   projectId: "beuka-backend",
 };
 
+/**
+ * Starter opp firebase backenden
+ */
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
 const app = express();
@@ -14,12 +21,20 @@ app.use(express.json());
 
 const PORT = 3000;
 
+/**
+ * Enkelt endepunkt som tar imot en fil og lagrer den i firebase storage.
+ */
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.get("/test/upload_sliten", (_req, res) => {
-  const storageRef = ref(storage, "assets/sliten_haakon.png");
-  /* uploadBytes(storageRef).then((snapshot) => {
-    console.log("Uploaded a blob or file!");
-    res.send(snapshot);
-  }); */
+  const file = readFileSync(__dirname + "/assets/sliten_haakon.png");
+  const storageRef = ref(storage, "sliten_haakon.png");
+  uploadBytes(storageRef, file)
+    .then(() => {
+      res.send("Uploaded");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 app.listen(PORT, () => {
