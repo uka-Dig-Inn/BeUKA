05506f0d5c41f2e3a3b89edb6350be4dcb78323d
Håkon <haakon.telje@gmail.com>
2023-01-17 19:58:15 +0100

    launcher kamera, må testes på iphone

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -1,21 +1,80 @@
-import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Dimensions, Image, ImageBackground } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ImageBackground,
+  Button,
+} from "react-native";
 import { PostProps } from "../types/Post";
+import * as ImagePicker from "expo-image-picker";
 
+
+/**
+ * Komponent for å vise en post.
+ * Tar inn post data og displayer en post.
+ * Enkel kravspec:
+ * - Viser tittel og innhold
+ * - Laste inn bilde fra databasen
+ * - Hvis beUKA prompten ikke er skjedd enda, 
+ * - Vis UKA logo over bilde som er lastet inn
+ * - Vis isåfall en prompt om å ta bilde ved å trykke på UKA-logoen,
+ * - Når den trykkes på, skal man få de seksti sekundene på å ta et bilde
+ * - Her er grove docs å bruke : https://docs.expo.dev/versions/latest/sdk/imagepicker/
+ * 
+ * @param post - posten som tas inn
+ * 
+ * TODO
+ * - Fikse at ved å trykke på logo kickes countdown og 
+ * 
+ */
 const Post = (post: any) => {
+  
+  const [image, setImage] = useState(null);
+  
   useEffect(() => {
     console.log(post);
-  }, []);
+  }, [image]);
+
+
+  const openCamera = async () => {
+
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+      if (permissionResult.granted === false) {
+        alert("You've refused to allow this app to access your photos!");
+
+      } else {
+        const result: any = await ImagePicker.launchCameraAsync(
+          {
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            aspect: [4, 3],
+            quality: 1,
+          }
+        );
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+
+        return result;
+      }
+    }
 
   return (
     <View style={styles.item}>
       <Text style={styles.title}>{post.item.title}</Text>
       <Text style={styles.body}>{post.item.body}</Text>
-      <ImageBackground source={require("../assets/MEMES/hildeGcut_out.png")}>
+      <Button title="Pick an image from camera roll" onPress={openCamera}/>
+      <ImageBackground source={image ? {uri: image} : require("../assets/MEMES/hildeGcut_out.png")}>
+        {image ? null :
         <Image
-            style={styles.image}
-            source={require("../assets/VISJONSlogo_rød.png")}
-        ></Image>
+        style={styles.image}
+        source={require("../assets/VISJONSlogo_rød.png")}
+      ></Image>
+      }
       </ImageBackground>
     </View>
   );
